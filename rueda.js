ce4f4e089b6072741c91721aa9febe0f722c7d5c
Josep <josep.ssv@gmail.com>
2019-02-15 13:27:59 +0100

    Update and rename JS to rueda.js

---
+++ rueda.js
@@ -0,0 +1,95 @@
+<html><head><title></title>
+<link rel=stylesheet href="rueda.css"></link>
+<script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous">
+</script>
+<script src="rueda.js">
+</script>
+
+</head>
+<body>
+<div id="usuario">
+
+</div>
+<table class="data-table" cellpadding="4px" cellpadding="4px" width="95%">
+  <tr>
+    <th class="hora"></th>
+    <th>Lunes</th>
+    <th>Martes</th>
+    <th>Mi&eacute;rcoles</th>
+    <th>Jueves</th>
+    <th>Viernes</th>
+  </tr>
+  <tr>
+    <td class="hora">De a</td>
+    <td id="l1"></td>
+    <td id="m1"></td>
+    <td id="x1"></td>
+    <td id="j1"></td>
+    <td id="v1"></td>
+  </tr>
+  <tr>
+    <td class="hora">De a</td>
+    <td id="l2"></td>
+    <td id="m2"></td>
+    <td id="x2"></td>
+    <td id="j2"></td>
+    <td id="v2"></td>
+  </tr>
+  <tr>
+    <td class="hora"></td>
+    <td id=""></td>
+    <td id=""></td>
+    <td id=""></td>
+    <td id=""></td>
+    <td id=""></td>
+  </tr>
+  <tr>
+    <td class="hora">De a</td>
+    <td id="l3"></td>
+    <td id="m3"></td>
+    <td id="x3"></td>
+    <td id="j6"></td>
+    <td id=""></td>
+  </tr>
+  <tr>
+    <td class="hora">De a</td>
+    <td id="l4"></td>
+    <td id="m4"></td>
+    <td id="x4"></td>
+    <td id="j4"></td>
+    <td id="v4"></td>
+  </tr>
+  <tr>
+    <td class="hora"></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td class="hora">De a</td>
+    <td id="l5"></td>
+    <td id="m5"></td>
+    <td id="x5"></td>
+    <td id="j5"></td>
+    <td id="v5"></td>
+  </tr>
+  <tr>
+    <td class="hora">De a</td>
+    <td id="l6"></td>
+    <td id="m6"></td>
+    <td id="x6"></td>
+    <td id="j6"></td>
+    <td id="v6"></td>
+  </tr>
+</table>
+
+<div id="usuario">
+
+</div>
+</body>
+</html>
